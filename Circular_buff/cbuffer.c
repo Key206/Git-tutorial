a4a1d2c05c8d07ea300522ceaa947e146a9761b9
Khoa Nguyen Minh <khoanm206@gmail.com>
2024-01-02 22:31:39 +0700

    add files of cbuffer feature

---
+++ Circular_buff/cbuffer.c
@@ -0,0 +1,42 @@
+#include  "cbuffer.h"
+
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size){
+    cb->data = buf;
+    cb->size = size;
+    cb->reader = 0;
+    cb->writer = 0;
+    cb->overflow = 0;
+    cb->active = true;
+}
+void cb_clear(cbuffer_t *cb){
+    cb->reader = 0;
+    cb->writer = 0;
+    cb->overflow = 0;
+}
+uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes){
+    uint32_t receivedBytes;
+    for(receivedBytes = 0; receivedBytes <= nbytes; ++receivedBytes){
+        if(cb_data_count(cb) > 0){ 
+            *((uint8_t*)buf + receivedBytes) = *(cb->data + (cb->reader++));
+        }
+    }
+    return receivedBytes;
+}
+uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes){
+    uint32_t writtenBytes;
+    for(writtenBytes = 0; writtenBytes <= nbytes; ++writtenBytes){
+        if(cb_space_count(cb) > 0){
+            *(cb->data + (cb->writer++)) = *((uint8_t*)buf + writtenBytes);
+        }
+    }
+    return writtenBytes;
+}
+uint32_t cb_data_count(cbuffer_t *cb){
+    uint32_t data_count = cb->reader <= cb->writer ? (cb->writer - cb->reader) : 
+                                                     (cb->writer + cb->size - cb->reader);
+    return data_count;
+}
+uint32_t cb_space_count(cbuffer_t *cb){
+    uint32_t space_count = cb->size - cb_data_count(cb) - 1;
+    return space_count;
+}
